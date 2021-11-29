94f44f290fc052c131b7e3ef911bb1dfad3cf295
Dmitrii Zwei <zweidmitr@gmail.com>
2021-11-29 11:51:59 +0300

    3. Liquibase. Интеграционные тесты. [#95881]

---
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.tracker;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.sql.*;
+
+import java.util.Properties;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class SqlTrackerTest {
+
+    private static Connection connection;
+
+    @BeforeClass
+    public static void initConnection() {
+        try (InputStream in = SqlTrackerTest.class.getClassLoader()
+                .getResourceAsStream("test.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @AfterClass
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    @After
+    public void wipeTable() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("delete from item")) {
+            statement.execute();
+        }
+    }
+
+    @Test
+    public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertThat(tracker.findById(item.getId()), is(item));
+    }
+}
