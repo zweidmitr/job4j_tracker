e820c68e6f4f49f2612e4691931b4ab60c59de1a
Dmitrii Zwei <zweidmitr@gmail.com>
2021-11-29 12:04:12 +0300

    3. Liquibase. Интеграционные тесты. [#95881]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -1,27 +1,29 @@
 package ru.job4j.tracker;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import ru.job4j.tracker.Item;
 
 import java.io.InputStream;
-import java.sql.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 
 public class SqlTrackerTest {
 
-    private static Connection connection;
+    static Connection connection;
 
     @BeforeClass
     public static void initConnection() {
-        try (InputStream in = SqlTrackerTest.class.getClassLoader()
-                .getResourceAsStream("test.properties")) {
+        try (InputStream in = SqlTrackerTest.class.getClassLoader().getResourceAsStream("test.properties")) {
             Properties config = new Properties();
             config.load(in);
             Class.forName(config.getProperty("driver-class-name"));
@@ -29,6 +31,7 @@ public class SqlTrackerTest {
                     config.getProperty("url"),
                     config.getProperty("username"),
                     config.getProperty("password")
+
             );
         } catch (Exception e) {
             throw new IllegalStateException(e);
@@ -42,7 +45,7 @@ public class SqlTrackerTest {
 
     @After
     public void wipeTable() throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("delete from item")) {
+        try (PreparedStatement statement = connection.prepareStatement("delete from items")) {
             statement.execute();
         }
     }
