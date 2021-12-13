4feacb53099675b18b1374fa87a52df5f214ffce
Dmitrii Zwei <zweidmitr@gmail.com>
2021-12-13 20:31:15 +0300

    1. Профилирование. [#1591 #239461]

--- src/main/java/ru/job4j/tracker/ProfMemExp.java
+++ src/main/java/ru/job4j/tracker/ProfMemExp.java
@@ -1,13 +1,31 @@
 package ru.job4j.tracker;
 
+import java.util.concurrent.TimeUnit;
+
 public class ProfMemExp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MemTracker tracker = new MemTracker();
-        for (int i = 0; i < 1_000_000_000; i++) {
+        TimeUnit.SECONDS.sleep(20);
+        add(tracker);
+        System.out.println("ups");
+        TimeUnit.SECONDS.sleep(10);
+        delete(tracker);
+
+         System.out.println("done");
+    }
+
+    private static void add(MemTracker tracker) {
+        for (int i = 0; i < 1_000_000; i++) {
             Item item = new Item("item " + i);
             tracker.add(item);
             System.out.println(i);
         }
-        System.out.println("done");
+    }
+
+    private static void delete (MemTracker tracker) {
+        for (int i = 0; i < 1_000_000; i++) {
+            tracker.delete(i);
+            System.out.println(i);
+        }
     }
 }
