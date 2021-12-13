6e264ebab646d513beff47565593f245f30b1ea1
Dmitrii Zwei <zweidmitr@gmail.com>
2021-12-13 14:51:16 +0300

    3. Tracker - add init in StartUI

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -35,17 +35,20 @@ public class StartUI {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        Store tracker = new SqlTracker();
+        try (Store tracker = new SqlTracker()) {
+            tracker.init();
+            List<UserAction> actions = new ArrayList<>();
+            actions.add(new CreateAction(output));
+            actions.add(new ShowAllItemAction(output));
+            actions.add(new ReplaceItemAction(output));
+            actions.add(new DeleteItemAction(output));
+            actions.add(new FindByIdAction(output));
+            actions.add(new FindByNameAction(output));
+            actions.add(new ExitProgramAction());
 
-        List<UserAction> actions = new ArrayList<>();
-        actions.add(new CreateAction(output));
-        actions.add(new ShowAllItemAction(output));
-        actions.add(new ReplaceItemAction(output));
-        actions.add(new DeleteItemAction(output));
-        actions.add(new FindByIdAction(output));
-        actions.add(new FindByNameAction(output));
-        actions.add(new ExitProgramAction());
-
-        new StartUI(output).init(input, tracker, actions);
+            new StartUI(output).init(input, tracker, actions);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
